ac67747e8ac3140ab5667f64053f633d8951a156
lucianojamimbo <lucianojamimbo@gmail.com>
2020-05-15 20:35:37 +0100

    no more global variable bs
    too confusing having global variables declared from functions in another python file, now there are now more global variables at the cost of having to input more variables into functions

--- ljmnistfuncs.py
+++ ljmnistfuncs.py
@@ -7,12 +7,10 @@ def sigmoidderiv(x):
     return (sigmoid(x)*(1-sigmoid(x)))
 #create the required variables for our network
 def init(sizes):
-    global delta
-    global biases
-    global weights
     biases = [np.random.randn(y, 1) for y in sizes[1:]]
     weights = [np.random.randn(y, x) for x, y in zip(sizes[:-1], sizes[1:])]
     delta = list(np.zeros(len(sizes)-1)) #fill delta with 0s then convert to list
+    return weights, biases, delta
 #returns a list of all neuron activations, and z for pre-sigmoided activations
 def feedforwards(w, b, a):
     activations = []
@@ -23,12 +21,10 @@ def feedforwards(w, b, a):
         a = sigmoid(np.add(np.dot(zw, a), [item for sublist in zb for item in sublist]))
         activations.append(a)
         z.append(bs)
-    activations = np.array(activations)
-    z = np.array(z)
-    return activations, z
+    return np.array(activations), np.array(z)
 def cost(y, a):
     return np.sum(np.power(np.subtract(y, a), 2))
-def getdelta(activations, desiredoutput, z):    
+def getdelta(activations, desiredoutput, z, delta, weights):    
     delta[-1] = np.multiply(np.subtract(activations[-1], desiredoutput), sigmoidderiv(z[-1])) #error in the output layer
     i = 1 #start at one because error in output is already done
     while i < len(delta):
--- main.py
+++ main.py
@@ -1,15 +1,14 @@
-import numpy as np
 from loaddata import ld
-import ljmnistfuncs
+import ljmnistfuncs #import all the basic functions
 
 
 #make sure the functions work:
 do = [0.2,0.2] #a variable required for testing cost()
-ljmnistfuncs.init([2,2,2])
+weights, biases, delta = ljmnistfuncs.init([2,2,2])
 acti, presig = ljmnistfuncs.feedforwards(weights, biases, [0.1,0.2])
 data = ld()
-ljmnistfuncs.getdelta(acti, do, presig)
-print("cost", cost(do, acti[-1]))
+ljmnistfuncs.getdelta(acti, do, presig, delta, weights)
+print("cost", ljmnistfuncs.cost(do, acti[-1]))
 print("first entry in data", data[0])
 print("activations", acti)
 print("pre-sigmoid activations", presig)
