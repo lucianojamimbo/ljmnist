56398c46a7de2ea61732006b9abab09c226cb4c3
lucianojamimbo <lucianojamimbo@gmail.com>
2020-05-15 21:02:11 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from loaddata import ld
 import ljmnistfuncs #import all the basic functions
 
-
 #make sure the functions work:
 do = [0.2,0.2] #a variable required for testing cost()
 weights, biases, delta = ljmnistfuncs.init([2,2,2])
