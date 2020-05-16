c4d84124302b99c4cb32a68bbba7a029c1fe9513
lucianojamimbo <lucianojamimbo@gmail.com>
2020-05-16 14:20:03 +0100

    added nbnwzero from main

--- ljmnistfuncs.py
+++ ljmnistfuncs.py
@@ -35,3 +35,17 @@ def getdelta(activations, desiredoutput, z, delta, weights):
 #list to list of lists
 def ltlol(lst):
     return [[itm] for itm in lst]
+#get nabla_w_zero and nabla_b_zero
+def nbnwzero(weights, biases, delta, sizes):
+    acti, presig = feedforwards(weights, biases, np.zeros(sizes[0])) #run with a full zero input because we dont care about anything but the shapes
+    do = np.zeros(sizes[-1]) #doesnt need to mean anything so its all zeros, we just want the shapes
+    currentnablaw = []
+    currentnablab = []
+    getdelta(acti, do, presig, delta, weights)
+    for a, d in zip(acti, delta):
+        currentnablaw.append(np.transpose(np.matmul((ltlol(a)), np.transpose(ltlol(d)))))
+    for d in delta:
+        currentnablab.append(d)
+    nabla_w_zero = np.multiply(currentnablaw, 0)
+    nabla_b_zero = np.multiply(currentnablab, 0)
+    return nabla_b_zero, nabla_w_zero
