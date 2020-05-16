3897090e1bbcd9612ecef4bed03581c635bb6302
lucianojamimbo <lucianojamimbo@gmail.com>
2020-05-16 19:54:29 +0100

    changed some inputs to trainSGD
    and a couple comments

--- main.py
+++ main.py
@@ -4,12 +4,10 @@ import numpy as np
 import ljmnistfuncs as lmf #import all the basic functions
 from train import trainSGD
 np.random.seed(0) #make the program behave the same way every time it is run so that its easier to track the effect of changes to the code
-#init time
-sizes = [784,16,10]
+sizes = [784,16,10] #this decides the shape of our network.
 weights, biases, delta = lmf.init(sizes) #init the main variables we need
 data = ld() #only loading to test the loading function works, this data is unused *currently*
 np.random.shuffle(data) #randomise our data
 nabla_b_zero, nabla_w_zero = lmf.nbnwzero(weights, biases, delta, sizes) #get nabla_w_zero and nabla_b_zero
-#testing and training
-weights, biases = trainSGD(30, 10, 0.09, data, nabla_w_zero, nabla_b_zero, sizes, delta, weights, biases)
-test(10000, weights, biases)
+weights, biases = trainSGD(10, 20, 0.02, data, nabla_w_zero, nabla_b_zero, sizes, delta, weights, biases) #training time
+test(10000, weights, biases) #test time
