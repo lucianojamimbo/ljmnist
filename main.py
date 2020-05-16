c68015f1917aa3952d8119b019d9a347900a6c29
lucianojamimbo <lucianojamimbo@gmail.com>
2020-05-16 14:20:37 +0100

    removed a big block of code to to ljmnistfuncs.py
    the code for getting nabla_w_zero and nabla_b_zero didnt need to be in main

--- main.py
+++ main.py
@@ -3,17 +3,6 @@ from testaccuracy import test
 import numpy as np
 import ljmnistfuncs as lmf #import all the basic functions
 np.random.seed(0) #make the program behave the same way every time it is run so that its easier to track the effect of changes to the code
-
-
-
-
-
-
-
-
-
-
-
 def trainSGD(epochs, batch_size, eta):
     global weights
     global biases
@@ -55,36 +44,12 @@ def trainSGD(epochs, batch_size, eta):
             i+=1
         print("epoch {0} complete".format(epoch+1))
         epoch +=1
-
-
-
-
-
-
-
-
-
-
-
-
+#init time
 sizes = [784,16,10]
 weights, biases, delta = lmf.init(sizes) #init the main variables we need
 data = ld() #only loading to test the loading function works, this data is unused *currently*
 np.random.shuffle(data) #randomise our data
-#all this is required to get the shapes of nabla_w and nabla_b (bit of a hassle but no way around this)
-do = [0,0,0,0,0,0,0,0,0,0]
-acti, presig = lmf.feedforwards(weights, biases, np.zeros(sizes[0])) #run with a full zero input because we dont care about anything but the shapes
-desiredoutput = np.zeros(sizes[-1]) #doesnt need to mean anything so its all zeros, we just want the shapes
-currentnablaw = []
-currentnablab = []
-lmf.getdelta(acti, do, presig, delta, weights)
-for a, d in zip(acti, delta):
-    currentnablaw.append(np.transpose(np.matmul((lmf.ltlol(a)), np.transpose(lmf.ltlol(d)))))
-for d in delta:
-    currentnablab.append(d)
-nabla_w_zero = np.multiply(currentnablaw, 0)
-nabla_b_zero = np.multiply(currentnablab, 0)
-
+nabla_b_zero, nabla_w_zero = lmf.nbnwzero(weights, biases, delta, sizes) #get nabla_w_zero and nabla_b_zero
 #testing and training
 test(10000, weights, biases)
 trainSGD(5, 10, 0.09)
