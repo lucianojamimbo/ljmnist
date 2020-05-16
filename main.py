3d6de5cb1e3a83087b8484e35d06c179725cae14
lucianojamimbo <lucianojamimbo@gmail.com>
2020-05-16 14:34:07 +0100

    moved trainSGD into its own file
    this is either a good idea or a bad idea, but it works the same so its probably fine

--- main.py
+++ main.py
@@ -2,48 +2,11 @@ from loaddata import ld
 from testaccuracy import test
 import numpy as np
 import ljmnistfuncs as lmf #import all the basic functions
+from train import trainSGD
 np.random.seed(0) #make the program behave the same way every time it is run so that its easier to track the effect of changes to the code
-def trainSGD(epochs, batch_size, eta):
-    global weights
-    global biases
-    epoch = 0
-    while epoch < epochs:
-        i = 0
-        dataiter = 0
-        while i < 60000/batch_size:
-            #make nabla_w and nabla_b full of zeros in the correct shape:
-            nabla_w = nabla_w_zero
-            nabla_b = nabla_b_zero
-            #get a batch
-            btch = 0
-            batch = []
-            while btch < batch_size:
-                batch.append(data[dataiter])
-                btch+=1
-                dataiter+=1
-            #do stuff
-            for item in batch:
-                desiredoutput = np.zeros(sizes[-1])
-                desiredoutput[int(item[1])] = 1
-                currentnablaw = []
-                currentnablab = []
-                acti, presig = lmf.feedforwards(weights, biases, item[0])
-                lmf.getdelta(acti, desiredoutput, presig, delta, weights) #there is an issue with this somewhere
-                for a, d in zip(acti, delta):
-                    currentnablaw.append(np.transpose(np.matmul((lmf.ltlol(a)), np.transpose(lmf.ltlol(d)))))
-                for d in delta:
-                    currentnablab.append(d)
-                nabla_w = np.add(nabla_w, currentnablaw)
-                nabla_b = np.add(nabla_b, currentnablab)
-            #below changes the weights and biases according to nabla_b and nabla_w
-            weights -= np.dot((eta/batch_size), nabla_w)
-            nbi = 0
-            for nb in nabla_b:
-                biases[nbi] -= lmf.ltlol(np.dot((eta/batch_size), nb))
-                nbi+=1
-            i+=1
-        print("epoch {0} complete".format(epoch+1))
-        epoch +=1
+
+
+
 #init time
 sizes = [784,16,10]
 weights, biases, delta = lmf.init(sizes) #init the main variables we need
@@ -52,5 +15,5 @@ np.random.shuffle(data) #randomise our data
 nabla_b_zero, nabla_w_zero = lmf.nbnwzero(weights, biases, delta, sizes) #get nabla_w_zero and nabla_b_zero
 #testing and training
 test(10000, weights, biases)
-trainSGD(5, 10, 0.09)
+weights, biases = trainSGD(5, 10, 0.09, data, nabla_w_zero, nabla_b_zero, sizes, delta, weights, biases)
 test(10000, weights, biases)
---
+++ train.py
@@ -0,0 +1,44 @@
+from testaccuracy import test
+import numpy as np
+import ljmnistfuncs as lmf #import all the basic functions
+
+def trainSGD(epochs, batch_size, eta, data, nabla_w_zero, nabla_b_zero, sizes, delta, weights, biases):
+    epoch = 0
+    while epoch < epochs:
+        i = 0
+        dataiter = 0
+        while i < 60000/batch_size:
+            #make nabla_w and nabla_b full of zeros in the correct shape:
+            nabla_w = nabla_w_zero
+            nabla_b = nabla_b_zero
+            #get a batch
+            btch = 0
+            batch = []
+            while btch < batch_size:
+                batch.append(data[dataiter])
+                btch+=1
+                dataiter+=1
+            #do stuff
+            for item in batch:
+                desiredoutput = np.zeros(sizes[-1])
+                desiredoutput[int(item[1])] = 1
+                currentnablaw = []
+                currentnablab = []
+                acti, presig = lmf.feedforwards(weights, biases, item[0])
+                lmf.getdelta(acti, desiredoutput, presig, delta, weights) #there is an issue with this somewhere
+                for a, d in zip(acti, delta):
+                    currentnablaw.append(np.transpose(np.matmul((lmf.ltlol(a)), np.transpose(lmf.ltlol(d)))))
+                for d in delta:
+                    currentnablab.append(d)
+                nabla_w = np.add(nabla_w, currentnablaw)
+                nabla_b = np.add(nabla_b, currentnablab)
+            #below changes the weights and biases according to nabla_b and nabla_w
+            weights -= np.dot((eta/batch_size), nabla_w)
+            nbi = 0
+            for nb in nabla_b:
+                biases[nbi] -= lmf.ltlol(np.dot((eta/batch_size), nb))
+                nbi+=1
+            i+=1
+        print("epoch {0} complete".format(epoch+1))
+        epoch +=1
+    return weights, biases
