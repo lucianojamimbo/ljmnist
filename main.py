fb0060f79a34e22021c993d21ad66a809b3013ae
lucianojamimbo <lucianojamimbo@gmail.com>
2020-05-16 14:55:52 +0100

    more epochs

--- main.py
+++ main.py
@@ -4,9 +4,6 @@ import numpy as np
 import ljmnistfuncs as lmf #import all the basic functions
 from train import trainSGD
 np.random.seed(0) #make the program behave the same way every time it is run so that its easier to track the effect of changes to the code
-
-
-
 #init time
 sizes = [784,16,10]
 weights, biases, delta = lmf.init(sizes) #init the main variables we need
@@ -14,6 +11,5 @@ data = ld() #only loading to test the loading function works, this data is unuse
 np.random.shuffle(data) #randomise our data
 nabla_b_zero, nabla_w_zero = lmf.nbnwzero(weights, biases, delta, sizes) #get nabla_w_zero and nabla_b_zero
 #testing and training
-test(10000, weights, biases)
-weights, biases = trainSGD(5, 10, 0.09, data, nabla_w_zero, nabla_b_zero, sizes, delta, weights, biases)
+weights, biases = trainSGD(30, 10, 0.09, data, nabla_w_zero, nabla_b_zero, sizes, delta, weights, biases)
 test(10000, weights, biases)
